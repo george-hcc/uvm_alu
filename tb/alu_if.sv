8a6af1dd8eacd88de0679021f0c8198ff3d9b1c0
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-10-29 00:15:14 -0300

    Criação de interface de ULA

---
+++ tb/alu_if.sv
@@ -0,0 +1,14 @@
+interface alu_if
+  (
+   input logic clk,
+   input logic rst_n
+  );
+
+   logic 	clk;
+   logic 	rst_n;
+   logic [31:0] data_A;
+   logic [1:0] 	reg_sel;
+   logic [1:0] 	instr;
+   logic 	valid;
+
+endinterface
