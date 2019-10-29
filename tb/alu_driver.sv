87a529ebecda433dc0a932bf8cb8a07e874f0b1a
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-10-29 19:29:56 -0300

    Correção e adição de tasks ao alu_driver

--- tb/alu_driver.sv
+++ tb/alu_driver.sv
@@ -3,7 +3,7 @@ typedef virtual alu_if alu_vif;
 class alu_driver extends uvm_driver #(alu_transaction);
    `uvm_object_utils(alu_driver)
    alu_vif vif;
-   event begin_record, end_record;
+   alu_transaction tr;
    
    function new(string name = "alu_driver", uvm_component parent = null);
       super.new(name, parent);
@@ -11,26 +11,31 @@ class alu_driver extends uvm_driver #(alu_transaction);
 
    virtual function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      assert(uvm_config_db#(alu_vif)::get(this, "", "vif", vif));
+      if(!uvm_config_db#(alu_vif)::get(this, "", "vif", vif))
+	`uvm_fatal("NOVIF", "failed to get virtual interface")
    endfunction // build_phase
 
    task run_phase(uvm_phase phase);
-      fork
-	 reset_signals();
-	 get_and_drive(phase);
-      join
+      get_and_drive(phase);
    endtask // run_phase
 
-   virtual task reset_signals();
-
-   endtask // reset_signals
-
-   virtual task get_and_drive();
-
+   virtual task get_and_drive(uvm_phase phase);
+      wait (alu_vif.rst_n === 0);
+      @(posedge alu_vif.rst_n);
+      forever begin
+	 seq_item_port.get_next_item(tr);
+	 driver_transfer(tr);
+	 seq_item_port.item_done();
+      end
    endtask // get_and_drive
 
-   virtual task driver_transfer();
-
+   virtual task driver_transfer(alu_transaction tr);
+      @(posedge alu_vif.clk);
+      alu_vif.data_A   <= tr.data_in;
+      alu_vif.reg_sel  <= tr.reg_sel;
+      alu_vif.instr    <= tr.instr;
+      alu_vif.valid_in <= tr.valid_in;
+      @(posedge alu_vif.valid_out);
    endtask // driver_transfer
-
+   
 endclass // alu_driver
