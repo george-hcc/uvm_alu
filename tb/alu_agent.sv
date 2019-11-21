ce0f94fd6f48e958be3637159ffc88f976efa3b4
George Camboim <george.camboim@embedded.ufcg.edu.br>
2019-11-20 23:05:48 -0300

    Correção de agent pós gambiarra

--- tb/alu_agent.sv
+++ tb/alu_agent.sv
@@ -7,10 +7,12 @@ class alu_agent extends uvm_agent;
    alu_monitor alu_mon;
 
    uvm_analysis_port #(alu_transaction) alu_agt_port;
+   uvm_analysis_port #(ref_transaction) ref_agt_port;
    
    function new(string name = "alu_agent", uvm_component parent = null);
       super.new(name, parent);
       alu_agt_port = new("alu_agt_port", this);
+      ref_agt_port = new("ref_agt_port", this);
    endfunction // new
 
    virtual function void build_phase(uvm_phase phase);
@@ -22,8 +24,9 @@ class alu_agent extends uvm_agent;
 
    virtual function void connect_phase(uvm_phase phase);
       super.connect_phase(phase);
-      alu_mon.alu_mon_port.connect(alu_agt_port);
-      alu_drv.seq_item_port.connect(sqr.seq_item_export);
+      alu_mon.alu_mon_port.connect(alu_agt_port); // mon -> refmod/coverage
+      ref_mon.alu_ref_port.connect(ref_agt_port); // mon -> comparator
+      alu_drv.seq_item_port.connect(sqr.seq_item_export); // sqr -> drv
    endfunction // connect_phase
 
 endclass // alu_agent
